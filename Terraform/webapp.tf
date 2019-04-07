88fb0db905e93a4c61ba146cb53853d2efc78df2
JorTurFer <jorge_turrado@hotmail.es>
2019-04-07 17:34:27 +0200

    añado nombre para el plan

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -1,7 +1,8 @@
 module "linuxwebapp" {
-  source                  = "./module/webapp"
-  name                    = "${azurerm_resource_group.terraformGroup.name}"
-  location                = "${azurerm_resource_group.terraformGroup.location}"
-  resource_group_name     = "${azurerm_resource_group.terraformGroup.name}"
+  source                 = "./module/webapp"
+  name                   = "${azurerm_resource_group.terraformGroup.name}"
+  location               = "${azurerm_resource_group.terraformGroup.location}"
+  resource_group_name    = "${azurerm_resource_group.terraformGroup.name}"
+  service_plan_name      = "${azurerm_resource_group.terraformGroup.name}"
   conection_string_value = "Server=${azurerm_sql_server.sqlServer.fully_qualified_domain_name};Initial Catalog=${azurerm_sql_database.sqlServerDb.name};User Id=${var.SQL_ADMIN_ID};Password=${var.SQL_PASSWORD};"
 }
